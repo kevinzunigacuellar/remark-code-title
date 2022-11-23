50e7bd686d1df06728d7a7f54cf7b5562b1f300d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-23 16:19:00 -0500

    ts check for null index and parent

--- packages/remark-code-title/src/remarkPlugin.ts
+++ packages/remark-code-title/src/remarkPlugin.ts
@@ -11,18 +11,20 @@ export const remarkCodeTitle: unified.Plugin<[], mdast.Root> = () => {
         if (!title) {
           if (/\stitle/.test(node.meta))
             console.warn(
-              `remark-code-title: title attribute is could not be parsed from meta string: ${node.meta}. Please use title="title" format.`
+              `remark-code-title: title attribute could not be parsed from meta string: ${node.meta}. Please use title="title" format.`
             );
           return;
         }
-
         const titleNode: mdast.HTML = {
           type: "html",
           value: `<div class="remark-code-title">${title}</div>`,
         };
-        parent?.children.splice(index as number, 0, titleNode);
-        return (index as number) + 2;
+
+        if (parent && parent.children && index) {
+          parent.children.splice(index, 0, titleNode);
+          return index + 2;
+        }
       });
     }
-  }
+  };
 };
--- packages/remark-code-title/test/code.test.ts
+++ packages/remark-code-title/test/code.test.ts
@@ -14,5 +14,26 @@ test("adds a div containing the title before a code node", async () => {
     `.trim();
   const processor = remark().use(plugin);
   const result = await processor.process(md);
-  expect(result.toString()).toContain(`<div class="remark-code-title">hello.js</div>`);
+  expect(result.toString()).toContain(
+    `<div class="remark-code-title">hello.js</div>`
+  );
+});
+
+test("adds a div containing the title before a code node inside a blockquote", async () => {
+  const md = `
+  # Some code in a blockquote
+
+  ># My cool code
+  >
+  >\`\`\`js title="hello.js"
+  >  console.log("hello world");
+  >\`\`\`
+  
+  Some block of text
+    `.trim();
+  const processor = remark().use(plugin);
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(
+    `<div class="remark-code-title">hello.js</div>`
+  );
 });
