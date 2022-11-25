f93d5df2577bddea3be78ef080707ab73060b61b
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-25 04:09:43 -0500

    handle edge cases

--- packages/remark-code-title/src/remarkPlugin.ts
+++ packages/remark-code-title/src/remarkPlugin.ts
@@ -5,12 +5,23 @@ import type * as unified from "unified";
 export const remarkCodeTitle: unified.Plugin<[], mdast.Root> = () => {
   return (tree, file) => {
     visit(tree, "code", (node, index, parent) => {
-      if (!node.meta) return;
+      const metaString = `${node.lang ?? ""} ${node.meta ?? ""}`.trim();
+      /* meta string is empty */
+      if (!metaString) return;
 
-      const [title] = node.meta.match(/(?<=title=("|'))(.*?)(?=("|'))/) ?? "";
+      /* when no language is specified but there is a meta string, 
+         by default node.lang has the node.meta value (considering there is not spaces) 
+      */
+
+      if (node.lang && node.lang.includes("title=")) {
+        node.meta = metaString;
+        node.lang = "plaintext";
+      }
+
+      const [title] = metaString.match(/(?<=title=("|'))(.*?)(?=("|'))/) ?? "";
 
       if (!title) {
-        if (node.meta.includes("title=")) {
+        if (metaString.includes("title=")) {
           file.message("Invalid title", node, "remark-code-title");
         }
         return;
@@ -18,7 +29,9 @@ export const remarkCodeTitle: unified.Plugin<[], mdast.Root> = () => {
 
       const titleNode: mdast.HTML = {
         type: "html",
-        value: `<div data-remark-code-title>${title}</div>`,
+        value: `<div data-remark-code-title data-language="${
+          node.lang ?? "plaintext"
+        }">${title}</div>`,
       };
 
       if (parent && parent.children && typeof index === "number") {
