1a402b5db9a652e125aaa7a3f711fce83cbcd654
marco-perin <30049458+marco-perin@users.noreply.github.com>
2023-10-25 16:41:11 -0700

    Update tests with the options

--- packages/remark-code-title/test/code.test.ts
+++ packages/remark-code-title/test/code.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "vitest";
 import codeTitle from "../src/index";
 import { remark } from "remark";
+import html from "remark-html";
 
 test("adds a title before a code node", async () => {
   const title = "random.js";
@@ -62,4 +63,195 @@ test("adds a title before a code node that does not have a language with a two w
   const processor = remark().use(codeTitle);
   const result = await processor.process(md);
   expect(result.toString()).toContain(title);
+});
+
+test("tests html execution", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const processor = remark().use(codeTitle).use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  expect(result.toString()).toContain("<div data-remark-code-title");
+  expect(result.toString()).not.toContain(
+    '<div class="data-remark-code-title"'
+  );
+});
+
+test("tests html execution using custom element", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const baseName = "custom-base-element";
+
+  const processor = remark()
+    .use(codeTitle, { baseName: baseName })
+    .use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  expect(result.toString()).toContain(`<div ${baseName}`);
+  // expect(result.toString()).not.toContain('<div data-remark-code-title');
+});
+
+test("tests html execution using an additional class as string", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const baseName = "custom-base-element";
+  const additionalClasses = "add-cls-1";
+
+  const processor = remark()
+    .use(codeTitle, {
+      baseName: baseName,
+      additionalClasses: additionalClasses,
+    })
+    .use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  // expect(result.toString()).toContain('<div data-remark-code-title');
+  expect(result.toString()).toContain(
+    `<div ${baseName} class="${additionalClasses}"`
+  );
+});
+
+test("tests html execution using an additional class as string array", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const baseName = "custom-base-element";
+  const additionalClasses = ["add-cls-1", "add-cls-2"];
+
+  const processor = remark()
+    .use(codeTitle, {
+      baseName: baseName,
+      additionalClasses: additionalClasses,
+    })
+    .use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  // expect(result.toString()).toContain('<div data-remark-code-title');
+  expect(result.toString()).toContain(
+    `<div ${baseName} class="${additionalClasses.join(" ")}"`
+  );
+});
+test("tests html execution using class instead of attribute", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const baseName = "custom-base-element";
+
+  const processor = remark()
+    .use(codeTitle, { useClassInsteadOfAttribute: true, baseName: baseName })
+    .use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  expect(result.toString()).not.toContain("<div data-remark-code-title");
+  expect(result.toString()).toContain(`<div class="${baseName}"`);
+});
+
+test("tests html execution using class instead of attribute with an additional class as string", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const baseName = "custom-base-element";
+  const additionalClasses = "add-cls";
+
+  const processor = remark()
+    .use(codeTitle, {
+      useClassInsteadOfAttribute: true,
+      baseName: baseName,
+      additionalClasses: additionalClasses,
+    })
+    .use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  expect(result.toString()).not.toContain("<div data-remark-code-title");
+  expect(result.toString()).toContain(
+    `<div class="${baseName} ${additionalClasses}"`
+  );
+});
+
+test("tests html execution using class instead of attribute with an additional class as string array", async () => {
+  const title = "random.js";
+  const md = `
+  # Some title
+  
+  \`\`\`js title="${title}"
+  console.log("hello world");
+  \`\`\`
+  
+  Some block of text
+  `.trim();
+
+  const baseName = "custom-base-element";
+  const additionalClasses = ["add-cls-1"];
+
+  const processor = remark()
+    .use(codeTitle, {
+      useClassInsteadOfAttribute: true,
+      baseName: baseName,
+      additionalClasses: additionalClasses,
+    })
+    .use(html, { sanitize: false });
+
+  const result = await processor.process(md);
+  expect(result.toString()).toContain(title);
+  expect(result.toString()).not.toContain("<div data-remark-code-title");
+  expect(result.toString()).toContain(
+    `<div class="${baseName} ${additionalClasses}"`
+  );
 });
